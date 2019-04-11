64a5f11a5ec966d1a45b19d09ece6b399dd94096
Александр <zinovyevaleks@gmail.com>
2019-04-11 18:18:30 +0700

    портфолио, преимущества, Workflow

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,17 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+	// переворачивающиеся карточки
 	$(".flipcard").flip({
 		axis: 'y',
 		trigger: 'hover'
-	  });
+	});
+
+	//всплывание по наведению
+	$('.portfolio-card-link').hide();
+
+	$('.portfolio-card').hover(function () {
+		$(this).find('.portfolio-card-link').slideDown();
+	}, function () {
+		$(this).find('.portfolio-card-link').slideUp();
+	});
 });
