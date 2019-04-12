7b3c66af9ded5e9e923ae3451b03c7e6fd9619cc
Александр <zinovyevaleks@gmail.com>
2019-04-12 16:09:08 +0700

    слайды с примерами, прокрутка до якоря

--- app/js/common.js
+++ app/js/common.js
@@ -14,4 +14,28 @@ $(function () {
 	}, function () {
 		$(this).find('.portfolio-card-link').slideUp();
 	});
+
+
+	//переключение примеров
+	$("#lounge").click(function () {
+		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-1.jpg)');
+	});
+	$("#children").click(function () { 
+		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-2.jpg)');
+	});
+	$("#kitchen").click(function () { 
+		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-3.jpg)');
+	});
+	$("#bathroom").click(function () { 
+		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-4.jpg)');
+	});
+
+	  //Плавная прокрутка до якоря
+	  $(".anchor_link").click(function() {
+		var _href = $(this).attr("href");
+		$("html, body").animate({
+		  scrollTop: $(_href).offset().top
+		});
+		return false;
+	  });
 });
