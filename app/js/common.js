c4b138c8ddfab454aa0de54bfdf477c1a2b68499
Александр <zinovyevaleks@gmail.com>
2019-04-30 12:33:50 +0700

    Переделал шапку и навбар

--- app/js/common.js
+++ app/js/common.js
@@ -37,18 +37,18 @@ $(function () {
 
 
 	//переключение примеров
-	$("#lounge").click(function () {
-		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-1.jpg)');
-	});
-	$("#children").click(function () { 
-		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-2.jpg)');
-	});
-	$("#kitchen").click(function () { 
-		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-3.jpg)');
-	});
-	$("#bathroom").click(function () { 
-		$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-4.jpg)');
-	});
+	// $("#lounge").click(function () {
+	// 	$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-1.jpg)');
+	// });
+	// $("#children").click(function () { 
+	// 	$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-2.jpg)');
+	// });
+	// $("#kitchen").click(function () { 
+	// 	$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-3.jpg)');
+	// });
+	// $("#bathroom").click(function () { 
+	// 	$(".examples-slide").css('background-image', 'url(../img/desktop/examples_slide-4.jpg)');
+	// });
 
 	  //Плавная прокрутка до якоря
 	  $(".anchor_link").click(function() {
